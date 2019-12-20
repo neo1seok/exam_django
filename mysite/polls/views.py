d20040ed289232f04fcfc77d2f1728e01a0f24ff
neo1seok <neo1seok@gmail.com>
2019-12-20 16:22:05 +0900

    add 장고 걸 튜터리얼

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,56 @@
-from django.shortcuts import render, get_object_or_404
-
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from .models import Question,Choice
-from django.shortcuts import render
-from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+from django.utils import timezone
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    # def get_queryset(self):
+    #     """Return the last five published questions."""
+    #     return Question.objects.order_by('-pub_date')[:5]
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
     return HttpResponse(output)
     return HttpResponse("Hello, world. You're at the polls index.")
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -33,9 +63,11 @@ def detail(request, question_id):
     #
     # return HttpResponse("You're looking at question %s." % question_id)
 
+
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
